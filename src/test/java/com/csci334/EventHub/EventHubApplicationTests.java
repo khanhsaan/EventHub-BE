8c96cd220718605a3a30ab762b73c76ee242af09
pyaephyokyaw9191 <pyaephyokyaw.91910@gmail.com>
2025-06-06 18:32:58 +1000

    mvn test available

--- src/test/java/com/csci334/EventHub/EventHubApplicationTests.java
+++ src/test/java/com/csci334/EventHub/EventHubApplicationTests.java
@@ -2,12 +2,13 @@ package com.csci334.EventHub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class EventHubApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
